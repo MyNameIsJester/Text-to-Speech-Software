f04763acd87bfda8f1b68851df1aa17774f795d0
Majori2410 <doanduongthuylinhbn@gmail.com>
2026-03-25 17:38:45 +0700

    Add Backend in project

---
+++ AudioGuideAPI/Controllers/POIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AudioGuideAPI.Data;
+using AudioGuideAPI.Models;
+
+namespace AudioGuideAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]                         //endoint to /api/poi
+    public class POIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<POI>> Get()         //Danh sách POI
+        {
+            var pois = FakeData.GetPOIs();
+            return Ok(pois);
+        }
+    }
+}
